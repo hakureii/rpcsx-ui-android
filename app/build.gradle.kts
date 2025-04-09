50c3a67da03cc2b1e5ccb8c553e2169ef4c06468
rei <131262408+hakureii@users.noreply.github.com>
2025-04-09 07:25:58 +0000

    set minimum sdk to 30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "net.rpcsx"
-        minSdk = 31
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
